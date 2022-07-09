b93302c8bde325924e5ce861cf6866f09a7b3aa5
Michael Cruz <84043530+carnivalcruz380@users.noreply.github.com>
2022-07-09 17:16:26 -0400

    Update MClist.hpp

--- header implementations/MClist.hpp
+++ header implementations/MClist.hpp
@@ -26,8 +26,6 @@ template <typename T>
 typename MClist<T>::const_MCiterator& MClist<T>::const_MCiterator::operator++(){
 	if (data->next != nullptr)
 		data = data->next;
-	else 
-		data = nullptr;
 	return (*this);
 }
 
@@ -36,25 +34,19 @@ typename MClist<T>::const_MCiterator  MClist<T>::const_MCiterator::operator++(in
 	const_MCiterator old = *this;	
 	if (data->next != nullptr)
 		data = data->next;
-	else 
-		data = nullptr;
 	return old;
 }
 template <typename T>
 typename MClist<T>::const_MCiterator& MClist<T>::const_MCiterator::operator--(){
-	if (data->next != nullptr)
+	if (data->prev != nullptr)
 		data = data->prev;
-	else
-		data = nullptr;
 	return (*this);
 }
 template <typename T>
 typename MClist<T>::const_MCiterator  MClist<T>::const_MCiterator::operator--(int){
 	const_MCiterator old = *this;
-	if (data->next != nullptr)
+	if (data->prev != nullptr)
 		data = data->prev;
-	else 
-		data = nullptr;
 	return old;
 }
 
@@ -74,9 +66,7 @@ bool MClist<T>::const_MCiterator::operator!=(const const_MCiterator& rhs)const{
 
 // ITERATOR 
 template <typename T>
-MClist<T>::MCiterator::MCiterator(): const_MCiterator{}{
-	const_MCiterator::data = nullptr;
-}
+MClist<T>::MCiterator::MCiterator(): const_MCiterator{}{}
 template <typename T>
 MClist<T>::MCiterator::MCiterator(Node* node): const_MCiterator{node}{}
 template <typename T>
@@ -92,8 +82,6 @@ template <typename T>
 typename MClist<T>::MCiterator& MClist<T>::MCiterator::operator++(){
 	if (const_MCiterator::data->next != nullptr)
 		const_MCiterator::data = const_MCiterator::data->next;
-	else 
-		const_MCiterator::data = nullptr;
 	return (*this);
 }
 template <typename T>
@@ -101,26 +89,19 @@ typename MClist<T>::MCiterator  MClist<T>::MCiterator::operator++(int){
 	MCiterator old = *this;
 	if (const_MCiterator::data->next != nullptr)
 		const_MCiterator::data = const_MCiterator::data->next;
-	else 
-		const_MCiterator::data = nullptr;
 	return old;
 }
 template <typename T>
 typename MClist<T>::MCiterator& MClist<T>::MCiterator::operator--(){
 	if (const_MCiterator::data->prev != nullptr)
 		const_MCiterator::data = const_MCiterator::data->prev;
-	else 
-		const_MCiterator::data = nullptr;
 	return (*this);
 }
 template <typename T>
 typename MClist<T>::MCiterator  MClist<T>::MCiterator::operator--(int){
 	MCiterator old = *this;
-	if (const_MCiterator::data->prev != nullptr){
+	if (const_MCiterator::data->prev != nullptr)
 		const_MCiterator::data = const_MCiterator::data->prev;
-	}
-	else 
-		const_MCiterator::data = nullptr;
 	return old;
 }
 
@@ -130,38 +111,48 @@ typename MClist<T>::MCiterator  MClist<T>::MCiterator::operator--(int){
 // LIST
 template <typename T>
 MClist<T>::MClist(){
-	head = nullptr;
-	tail = nullptr;
+	head = new Node();
+	tail = new Node();
 	size = 0;
 }
 template <typename T>
-MClist<T>::MClist(const MClist<T>&){
-	
+MClist<T>::MClist(const MClist<T>& l){
+	head = new Node(l.head);
+	tail = new Node(l.tail);
+	size = l.size;
+	auto itr = l.begin();
+	while(itr != l.end()){
+		push_back(itr.data);
+		itr++;
+	};
 }
 template <typename T>
 MClist<T>& MClist<T>::operator=(const MClist<T>& l){
 	if (size > 0){
-		while (size > 0){
-			auto tempNode = pop_back();
-			delete tempNode;
+		while (pop_back()){
+			continue;
 		};
+		delete head;
+		delete tail;
 	}
 	head = new Node(l.head);
+	tail = new Node(l.tail);
 	size = l.size;
 	auto itr = l.begin();
-	itr++;
-	while (itr != l.end()){
-		push_back(new Node(itr.data));
+	while (itr != nullptr){
+		itr = push_back(new Node(itr.data));
 		itr++;
 	};
-	tail = 
 	return (*this);
 }
 template <typename T>
 MClist<T>::~MClist(){
 	while (size > 0){
-		auto tempNode = pop_back();
-		delete tempNode;		
+		while(pop_back()){
+			continue;
+		};
+		delete head;
+		delete tail;		
 	};
 }
 
@@ -171,94 +162,121 @@ MClist<T>::MClist(T val, int copies){}
 template <typename T>			
 MClist<T>::MClist(MClist<T>&& l){
 	head = std::move(l.head);
+	tail = std::move(l.tail);
+	size = std::move(l.size);
 	auto itr = l.begin();
-	itr++;
 	while (itr != l.end()){
 		push_back(std::move(itr.data));
-		itr++;
+		++itr;
 	};
 }
 template <typename T>
 MClist<T>& MClist<T>::operator=(MClist<T>&& l){
 	if (size > 0){
-		while (size > 0){
-			auto tempNode = pop_back();
-			delete tempNode;
+		while (pop_back()){
+			continue;
 		};
+		delete head;
+		delete tail;
 	}
-	auto itr = l.begin();
-	while (itr != l.end()){
-		push_back(new Node(std::move(itr.data)));
-		itr++;
-	};
 	head = std::move(l.head);
 	tail = std::move(l.tail);
 	size = std::move(l.size);
+	auto itr = l.begin();
+	while (itr != l.end()){
+		push_back(std::move(itr.data));
+		++itr;
+	};
 	return (*this);
 }
 			
 template <typename T>			
 void MClist<T>::push_back(T val){
-	Node* node = new Node(val, tail, nullptr);
-	tail->next = node;
-	tail = node;
-	size++;
-	if (size == 1)
-		head = tail;
-	
+	if (size > 0){
+		Node* node = new Node(val, tail->prev, tail);
+		tail->prev->next = node;
+		tail->prev = node;
+	}
+	else
+		head->next = tail->prev = new Node(val, head, tail);
+	++size;
 }
 template <typename T>
 void MClist<T>::push_front(T val){
-	Node* node = new Node(val, nullptr, head);
-	head->prev = node;
-	head = node; 
-	size++;
-	if (size == 1)
-		tail = head;
-}
-template <typename T>
-typename MClist<T>::Node* MClist<T>::pop_back(){
 	if (size > 0){
-		Node* node = tail;
-		tail = node->prev;
-		size--;
-		return node;
+		Node* node = new Node(val, head, head->next);
+		head->next->prev = node;
+		head->next = node;
 	}
 	else
-		return nullptr;
+		tail->prev = head->next = new Node(val, head, tail);
+	++size;
 }
 template <typename T>
-typename MClist<T>::Node* MClist<T>::pop_front(){
-	if (size > 0){
-		Node* node = head;
-		head = node->next;
-		size--;
-		return node;
+bool MClist<T>::pop_back(){
+	Node* node = tail->prev;
+	if (size > 1){
+		node->prev->next = tail;
+		tail->prev = node->prev;
+		delete node;
+		--size;
+		return true;
+	}
+	else if (size == 1){
+		tail->prev = nullptr;
+		head->next = nullptr;
+		delete node;
+		--size;
+		return true;
+	}
+	else
+		return false;
+}
+template <typename T>
+bool MClist<T>::pop_front(){
+	Node* node = head->next;
+	if (size > 1){
+		node->next->prev = head;
+		head->next = node->next;
+		delete node;
+		--size;
+		return true;
+	}
+	else if (size == 1){
+		head->next = nullptr;
+		tail->prev = nullptr;
+		delete node;
+		--size;
+		return true;
 	}
 	else 
-		return nullptr;
+		return false;
 
 }
 		
 template <typename T>			
 typename MClist<T>::MCiterator MClist<T>::begin()const{
-	return MCiterator(head);
+	auto itr = MCiterator(head->next);
+	return itr;
 }
 template <typename T>
 typename MClist<T>::MCiterator MClist<T>::end()const{
-	return MCiterator();
+	auto itr = MCiterator(tail);
+	return itr;
 }
 
 template <typename T>			
 typename MClist<T>::MCiterator MClist<T>::insert(MCiterator it, T val){}
 template <typename T>
 void MClist<T>::print()const{
-	auto itr = begin();
-	while (itr != end()){
-		std::cout << *itr << " ";
-		++itr;
+	if (size > 0){
+		auto itr = begin();
+		while (itr != end()){
+			std::cout << *itr << " ";
+			++itr;
+		};
+		std::cout << '\n';
 	}
-	std::cout << *itr << std::endl;
 }
 template <typename T>
 unsigned int MClist<T>::getSize()const{
